d06d4e71408faa68611bcf16c7d7e760b7328c06
vladiyudi <plastikwood@gmail.com>
2022-09-07 14:00:41 +0300

    migrates

--- Routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {signup} = require('../Controllers/userController')
-const {passwordMatch, validateNewUser}= require('../Middleware/userMiddleware')
-
-router.post('/signup', passwordMatch,  validateNewUser, signup)
-
-module.exports = router
