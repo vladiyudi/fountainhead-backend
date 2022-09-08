e41e7825c98c1134798a132bec1b0ae9111189d4
vladiyudi <plastikwood@gmail.com>
2022-09-08 15:53:44 +0300

    tables

---
+++ backend/Migrations/20220908124606_projects.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return  knex.schema.createTable('projects', (table) => {
+        table.increments('id').primary()
+        table.string('type')
+        table.string('name')
+        table.string('info')
+        table.string('picture')
+        table.string('link')
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+    }).then(() => console.log('Table created'))
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('projects')
+};
